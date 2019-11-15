3f6e1c5dcdf5dcb196e16426351df5eb275608e9
Dprash <prashantdixit600@gmail.com>
2019-11-16 02:22:39 +0530

    Text to Speech implemented

--- app/src/main/java/com/example/minorformsapp/SecondActivity.kt
+++ app/src/main/java/com/example/minorformsapp/SecondActivity.kt
@@ -2,9 +2,14 @@ package com.example.minorformsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_second.*
+import java.util.*
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var mTextToSpeech: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,36 @@ class SecondActivity : AppCompatActivity() {
 
         textView_form_name.text = name
         textView_form_url.text = url
-        button_form_description.text = description
+        textView_form_description.text = description
+
+        mTextToSpeech = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
+            if(it!=TextToSpeech.ERROR){
+                mTextToSpeech.language = Locale.ENGLISH
+            }
+        })
+
+        button_speak.setOnClickListener {
+            val toSpeech = textView_form_description.text.toString()
+            if(toSpeech==""){
+                Toast.makeText(baseContext,"Enter Text",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(baseContext,toSpeech,Toast.LENGTH_SHORT).show()
+                mTextToSpeech.speak(toSpeech,TextToSpeech.QUEUE_FLUSH,null)
+            }
+        }
+        button_stop.setOnClickListener {
+            if(mTextToSpeech.isSpeaking) {
+                mTextToSpeech.stop()
+            }else{
+                Toast.makeText(baseContext,"Not Speaking",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onPause() {
+        if(mTextToSpeech.isSpeaking) {
+            mTextToSpeech.stop()
+        }
+        super.onPause()
     }
 }
